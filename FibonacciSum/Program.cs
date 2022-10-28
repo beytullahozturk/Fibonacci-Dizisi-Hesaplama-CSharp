506b59606e6f4d529a19ee9d4d6f7267dbb1782b
Beytullah Öztürk <ozbeytullah2@outlook.com>
2022-10-28 14:25:13 +0300

    Add project files.

---
+++ FibonacciSum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FibonacciSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 1, c, sayi;
+            Console.Write("Hesaplanacak eleman sayısını girin: ");
+            sayi = int.Parse(Console.ReadLine());
+            Console.Write(a + " " + b + " "); 
+            for (int i = 2; i < sayi; ++i)
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
